2023d73233fca7801a0731a64315899c942b126b
Apoorva231 <156367670+Apoorva231@users.noreply.github.com>
2026-07-10 16:38:44 -0400

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isOperator(string s){
+        return s=="+" || s=="-" || s=="*" || s=="/";
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<string> st;
+        for(int i=0;i<tokens.size();i++){
+            if(!isOperator(tokens[i])){
+                st.push(tokens[i]);
+
+            }else{
+                int num1=stoi(st.top());
+                st.pop();
+                int num2=stoi(st.top());
+                st.pop();
+                int res;
+
+                if(tokens[i]=="+"){
+                    res=num1+num2;
+                }else if(tokens[i]=="-"){
+                    res= num2-num1;
+                }else if(tokens[i]=="*"){
+                    res= num1*num2;
+                }else{
+                    res= num2/num1;
+                }
+
+                st.push(to_string(res));
+        
+            }
+
+
+        }
+
+        int ans= stoi(st.top());
+
+        return ans;
+    }
+
+    
+};
